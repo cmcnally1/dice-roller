be577746796828b64409bd6892c26d428f70c4cb
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-03-05 18:27:05 +0000

    Added new text and button to main activity

--- app/src/main/java/com/cmcnally/diceroller/MainActivity.kt
+++ app/src/main/java/com/cmcnally/diceroller/MainActivity.kt
@@ -2,10 +2,25 @@ package com.cmcnally.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.cmcnally.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //View binding variable
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Using View binding to inflate the activity main layout
+        binding = ActivityMainBinding.bind(findViewById(R.id.root))
+
+        //Roll button used to roll the dice
+        val rollButton: Button = binding.button
+
+        //Changing the roll button text programmatically
+        rollButton.text = "Let's Roll"
     }
 }
